9f0df4c8f42448454f85afccac2f2510d5ef65aa
AjayKMehta <AjayMehta1@gmail.com>
2022-03-15 10:27:29 -0700

    :white_check_mark: Add test project.

---
+++ tests/BuilderBuilder.Tests/BuilderGeneratorSnapshotTests.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+using VerifyNUnit;
+
+namespace BuilderBuilder.Tests;
+
+public class BuilderGeneratorSnapshotTests
+{
+    public void ShouldGenerateCodeCorrectly()
+    {
+        Assert.Pass();
+    }
+}
