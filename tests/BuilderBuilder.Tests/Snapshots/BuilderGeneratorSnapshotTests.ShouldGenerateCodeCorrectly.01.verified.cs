887645d30c7f358b903ff2477ab276e9659e3678
AjayKMehta <AjayMehta1@gmail.com>
2022-03-16 08:02:02 -0700

    Add snapshots.

---
+++ tests/BuilderBuilder.Tests/Snapshots/BuilderGeneratorSnapshotTests.ShouldGenerateCodeCorrectly.01.verified.cs
@@ -0,0 +1,8 @@
+﻿//HintName: BuildableAttribute.g.cs
+
+using System;
+
+namespace BuilderBuilder;
+
+[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+public sealed class BuildableAttribute : Attribute { }
---
+++ tests/BuilderBuilder.Tests/Snapshots/BuilderGeneratorSnapshotTests.ShouldGenerateCodeCorrectly.02.verified.cs
@@ -0,0 +1,12 @@
+﻿//HintName: PointBuilder.cs
+using System;
+
+namespace <global namespace>;
+public class PointBuilder {
+public int X { get; set; }
+public int Y { get; set; }
+
+public Point Build() =>
+    new Point(X, Y);
+
+}
