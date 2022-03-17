de91259b7443da253f24f0e2e3b2fb021087bc81
AjayKMehta <AjayMehta1@gmail.com>
2022-03-17 16:14:50 -0700

    Move SetupDiffTool to ModuleInitializer.

--- tests/BuilderGenerator.Tests/ModuleInitializer.cs
+++ tests/BuilderGenerator.Tests/ModuleInitializer.cs
@@ -1,3 +1,5 @@
+using DiffEngine;
+using System.IO;
 using System.Runtime.CompilerServices;
 using VerifyTests;
 
@@ -9,5 +11,87 @@ public static class ModuleInitializer
     public static void Init()
     {
         VerifySourceGenerators.Enable();
+
+        SetupDiffTool();
+    }
+    private static void SetupDiffTool()
+    {
+        static string TargetLeftArguments(string temp, string target)
+        {
+            var tempTitle = Path.GetFileName(temp);
+            var targetTitle = Path.GetFileName(target);
+            return $"/u /ignoreeol /wl /e \"{target}\" \"{temp}\" /dl \"{targetTitle}\" /dr \"{tempTitle}\"";
+        }
+
+        static string TargetRightArguments(string temp, string target)
+        {
+            var tempTitle = Path.GetFileName(temp);
+            var targetTitle = Path.GetFileName(target);
+            return $"/u /ignoreeol /wl /e \"{temp}\" \"{target}\" /dl \"{tempTitle}\" /dr \"{targetTitle}\"";
+        }
+
+        var _ = DiffTools.AddTool(
+            name: "MyTool",
+            autoRefresh: true,
+            isMdi: false,
+            supportsText: true,
+            requiresTarget: true,
+            binaryExtensions: new[]
+            {
+                "bmp",
+                "cut",
+                "dds",
+                "exr",
+                "g3",
+                "gif",
+                "hdr",
+                "ico",
+                "iff",
+                "lbm",
+                "j2k",
+                "j2c",
+                "jng",
+                "jp2",
+                "jpg",
+                "jif",
+                "jpeg",
+                "jpe",
+                "jxr",
+                "wdp",
+                "hdp",
+                "koa",
+                "mng",
+                "pcd",
+                "pcx",
+                "pfm",
+                "pct",
+                "pict",
+                "pic",
+                "png",
+                "pbm",
+                "pgm",
+                "ppm",
+                "psd",
+                "ras",
+                "sgi",
+                "rgb",
+                "rgba",
+                "bw",
+                "tga",
+                "targa",
+                "tif",
+                "tiff",
+                "wap",
+                "wbmp",
+                "wbm",
+                "webp",
+                "xbm",
+                "xpm"
+            },
+            windows: new(
+                TargetLeftArguments,
+                TargetRightArguments,
+                @"D:\Apps\WinMerge\WinMergeU.exe")
+        );
     }
 }
--- tests/BuilderGenerator.Tests/TestHelper.cs
+++ tests/BuilderGenerator.Tests/TestHelper.cs
@@ -1,9 +1,7 @@
-using System.Collections.Generic;
-using System.IO;
-using System.Threading.Tasks;
-using DiffEngine;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using VerifyNUnit;
 
 namespace BuilderBuilder.Tests;
@@ -33,91 +31,8 @@ public static class TestHelper
 
         driver = driver.RunGenerators(compilation);
 
-        SetupDiffTool();
-
         return Verifier
             .Verify(driver)
             .UseDirectory("Snapshots");
-    }
-
-    private static void SetupDiffTool()
-    {
-        static string TargetLeftArguments(string temp, string target)
-        {
-            var tempTitle = Path.GetFileName(temp);
-            var targetTitle = Path.GetFileName(target);
-            return $"/u /ignoreeol /wl /e \"{target}\" \"{temp}\" /dl \"{targetTitle}\" /dr \"{tempTitle}\"";
-        }
-
-        static string TargetRightArguments(string temp, string target)
-        {
-            var tempTitle = Path.GetFileName(temp);
-            var targetTitle = Path.GetFileName(target);
-            return $"/u /ignoreeol /wl /e \"{temp}\" \"{target}\" /dl \"{tempTitle}\" /dr \"{targetTitle}\"";
-        }
-
-        var _ = DiffTools.AddTool(
-            name: "MyTool",
-            autoRefresh: true,
-            isMdi: false,
-            supportsText: true,
-            requiresTarget: true,
-            binaryExtensions: new[]
-            {
-                "bmp",
-                "cut",
-                "dds",
-                "exr",
-                "g3",
-                "gif",
-                "hdr",
-                "ico",
-                "iff",
-                "lbm",
-                "j2k",
-                "j2c",
-                "jng",
-                "jp2",
-                "jpg",
-                "jif",
-                "jpeg",
-                "jpe",
-                "jxr",
-                "wdp",
-                "hdp",
-                "koa",
-                "mng",
-                "pcd",
-                "pcx",
-                "pfm",
-                "pct",
-                "pict",
-                "pic",
-                "png",
-                "pbm",
-                "pgm",
-                "ppm",
-                "psd",
-                "ras",
-                "sgi",
-                "rgb",
-                "rgba",
-                "bw",
-                "tga",
-                "targa",
-                "tif",
-                "tiff",
-                "wap",
-                "wbmp",
-                "wbm",
-                "webp",
-                "xbm",
-                "xpm"
-            },
-            windows: new(
-                TargetLeftArguments,
-                TargetRightArguments,
-                @"D:\Apps\WinMerge\WinMergeU.exe")
-        );
     }
 }
